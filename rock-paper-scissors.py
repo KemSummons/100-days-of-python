90a9cfeaa7e063838aa7a7f952ea5133cdbfe213
Alexander Regel <kemsummons@gmail.com>
2022-08-20 19:45:32 +0300

    feat: game rock, paper, scissors

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,20 +1,48 @@
 import random
 
+rock = '''
+    _______
+---'     ____)
+        (_____)
+        (_____)
+        (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'     ____)____
+              ______)
+              _______)
+            _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'     ____)____
+              ______)
+         __________)
+        (____)
+---.__(___)
+'''
+
+
 player_choice = int(input("Enter '1' for rock, '2' for paper, '3' for scissors."))
 if player_choice == 1:
-    print('Rock')
+    print(rock)
 elif player_choice == 2:
-    print('Paper')
+    print(paper)
 elif player_choice == 3:
-    print('Scissors')
+    print(scissors)
 
 random_digit = random.randint(1, 3)
 if random_digit == 1:
-    print('Rock')
+    print(rock)
 elif random_digit == 2:
-    print('Paper')
+    print(paper)
 else:
-    print('Scissors')
+    print(scissors)
 
 if player_choice == 1 and random_digit == 1:
     print('Draw!')
@@ -32,5 +60,7 @@ elif player_choice == 3 and random_digit == 1:
     print('You lose!')
 elif player_choice == 3 and random_digit == 2:
     print('You win!')
-else:
+elif player_choice == 3 and random_digit == 3:
     print('Draw!')
+else:
+    print('You tap invalid command!')
