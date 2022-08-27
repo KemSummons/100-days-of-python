df61a0f0dd9980f285baa92b916202d6d3d82635
Alexander Regel <kemsummons@gmail.com>
2022-08-27 21:07:10 +0300

    feat: hangman game part 1

---
+++ day-7-hangman-game/hangman-game.py
@@ -0,0 +1,19 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+display = []
+for letter in chosen_word:
+    display += "_"
+print(display)
+
+while "_" in display:
+    guess = input('Guess a letter: ').lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+print('You win!')
