7088288a81c58e76d2a485eea29e3a462d791f7b
Alexander Regel <kemsummons@gmail.com>
2022-09-04 18:57:47 +0300

    feat: end task

--- day-8-function-parameters-and-caesar-cipher/caesar_cipher_light_version_best.py
+++ day-8-function-parameters-and-caesar-cipher/caesar_cipher_light_version_best.py
@@ -1,13 +1,7 @@
 import string
 from art import *
 
-print(logo)
 alphabet = list(string.ascii_lowercase)
-direction = input('Type "encode" to encrypt, type "decode" to decrypt:\n')
-while direction != 'encode' and direction != 'decode':
-    direction = input('Type "encode" to encrypt, type "decode" to decrypt:\n')
-text = input('Type your message:\n').lower()
-shift = int(input('Type the shift number:\n'))
 
 
 def caesar(text, shift, direction):
@@ -15,11 +9,27 @@ def caesar(text, shift, direction):
     if direction == 'decode':
         shift *= -1
     for letter in text:
-        index_letter = alphabet.index(letter)
-        shift_index_letter = (index_letter + shift) % 26
-        cipher_letter = alphabet[shift_index_letter]
-        final_text += cipher_letter
+        if letter not in alphabet:
+            final_text += letter
+        else:
+            index_letter = alphabet.index(letter)
+            shift_index_letter = (index_letter + shift) % 26
+            cipher_letter = alphabet[shift_index_letter]
+            final_text += cipher_letter
     print(f'The {direction} text is "{final_text}".')
 
 
-caesar(text, shift, direction)
+print(logo)
+
+continuation = True
+while continuation:
+    direction = input('Type "encode" to encrypt, type "decode" to decrypt:\n')
+    text = input('Type your message:\n').lower()
+    shift = int(input('Type the shift number:\n'))
+
+    caesar(text, shift, direction)
+
+    question = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if question == 'no':
+        continuation = False
+        print('Goodbye!')
