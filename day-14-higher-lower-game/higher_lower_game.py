f686ff605116dec04adc3045a9eb95262ae49800
Alexander Regel <kemsummons@gmail.com>
2022-10-02 14:27:03 +0300

    feat: end task 'higher lower game'

--- day-14-higher-lower-game/higher_lower_game.py
+++ day-14-higher-lower-game/higher_lower_game.py
@@ -3,39 +3,62 @@ import random
 from art import *
 from game_data import data
 
-random_first = random.choice(data)
-random_second = random.choice(data)
-print(logo)
-first_side = f"Side A: {random_first['name']}, a {random_first['description']}, from {random_first['country']}"
-print(first_side)
-print(vs)
-second_side = f"Side B: {random_second['name']}, a {random_second['description']}, from {random_second['country']}"
-print(second_side)
-question = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+def random_account():
+    return random.choice(data)
+
+
+def info(account):
+    account_name = account['name']
+    account_description = account['description']
+    account_country = account['country']
+    return f'{account_name}, a {account_description}, from {account_country}.'
+
+
+def answer(guess, a_followers, b_followers):
+    if guess == 'a':
+        if a_followers > b_followers:
+            return True
+        else:
+            return False
+    else:
+        if a_followers < b_followers:
+            return True
+        else:
+            return False
 
 
 def game():
-    count = 0
-    if question == 'a':
-        if random_first['follower_count'] > random_second['follower_count']:
-            print('You are right!')
-            count += 1
-            print(count)
+    print(logo)
+    score = 0
+    game_on = True
+    account_a = random_account()
+    account_b = random_account()
+
+    while game_on:
+        account_a = account_b
+        account_b = random_account()
+
+        while account_a == account_b:
+            account_b = random_account()
+
+        print(f"Side A: {info(account_a)}")
+        print(vs)
+        print(f"Side B: {info(account_b)}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_followers_count = account_a['follower_count']
+        b_followers_count = account_b['follower_count']
+        is_correct = answer(guess, a_followers_count, b_followers_count)
+
+        os.system('cls')
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f'You are right! Score now is: "{score}".')
         else:
-            os.system('cls')
-            print('You are not right!')
-            exit()
-    elif question == 'b':
-        if random_first['follower_count'] < random_second['follower_count']:
-            print('You are right!')
-            count += 1
-            print(count)
-        else:
-            os.system('cls')
-            print('You are not right!')
-            exit()
-    else:
-        print(f'Invalid enter! Final score: {count}')
+            game_on = False
+            print(f'You are now right! Final score: "{score}".')
 
 
 game()
